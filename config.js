77e52040b8a5ce6d52a10f8411a3aae9cffb394f
John Clayton Blanc <108766949+jcblanc2@users.noreply.github.com>
2023-11-01 09:28:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 export const apiKeys = {
-    REPLICATE_API_TOKEN: "r8_RoWotLvIoQw5y91uuRCNpGSlu9P9DFv2utIQ8",
-    OPENAI_API_KEY: "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",
-    API_TOKEN1: "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",
-    API_TOKEN2: "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",
+    REPLICATE_API_TOKEN: "",
+    OPENAI_API_KEY: "",
+    API_TOKEN1: "",
 }
