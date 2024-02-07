d25893347f32988cf779ce687818b6e04a731019
John Clayton Blanc <108766949+jcblanc2@users.noreply.github.com>
2024-02-07 13:04:54 -0500

    Update constant.js

--- api/constant.js
+++ api/constant.js
@@ -1,6 +1,6 @@
 let input = {
     prompt: null,
-    system_prompt: "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible. Your answers should not include any harmful.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+    system_prompt: "You are a helpful, respectful and honest assistant. If you don't know the answer to a question, please don't share false information.",
     min_new_tokens: -1
 };
 
